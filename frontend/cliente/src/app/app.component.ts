522529d04c00806ce6895b7646bb84036f37fc2c
Antonio Pulido Hernández <Antonio.Pulido@alu.uclm.es>
2019-12-03 10:22:32 +0100

    App movil

--- frontend/cliente/src/app/app.component.ts
+++ frontend/cliente/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+declare var device;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SGC-E1';
+export class AppComponent implements OnInit{
+ngOnInit() {
+document.addEventListener("deviceready", function() {
+alert(device.platform);
+}, false);
+}
 }
